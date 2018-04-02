773e5b4036337a4f6a262d4e26c066ae02a24bad
xumemgling <18838983960@163.com>
2018-04-02 12:05:06 +0800

    搭建尾部

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <router-view></router-view>
+      <FooterGuide></FooterGuide>
+
+    </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+  export default {
+    components:{
+      FooterGuide
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    width 100%
+    height 100%
+    position relative
+
+</style>
